ec57c2a7ab8a5fc7d37d68fdf73593e65e199ec2
RogeriaLeandro <RogeriaLeandro>
2023-09-05 16:05:37 -0300

    Criação do Projeto ArquivoPagamento

---
+++ src/main/java/br/com/arquivopagamento/ArquivopagamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.arquivopagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquivopagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquivopagamentoApplication.class, args);
+	}
+
+}
